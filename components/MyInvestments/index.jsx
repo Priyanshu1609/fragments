391550b9f055128cafe9389a020e4153ceed5e7f
xRAIDENx <priyanshupanda.ctp@gmail.com>
2022-09-18 18:44:02 +0530

    ui changes

--- components/MyInvestments/index.jsx
+++ components/MyInvestments/index.jsx
@@ -49,7 +49,7 @@ const MyInvestment = () => {
         <div className='h-[30rem]' >
             <div className='py-4 flex relative items-center justify-evenly'>
                 <div onClick={handlePrev} className='cursor-pointer  bg-gray-300 rounded-full p-2 absolute -left-12 top-60'><ChevronLeftIcon className='text-white h-7 w-7' /></div>
-                {/* <Swiper
+                <Swiper
                     ref={sliderRef}
                     // grabCursor={true}
                     slidesPerView={"auto"}
@@ -57,53 +57,53 @@ const MyInvestment = () => {
                     scrollbar={true}
                     modules={[Keyboard, Scrollbar, Navigation, Pagination]}
                     className="mySwiper"
-                > */}
+                >
 
                     {vaults?.map((vault) => (
                         vault.target != 0 &&
-                        // <SwiperSlide>
-                        //     <div key={vault.vaultAddress} className='cursor-pointer' onClick={() =>
-                        //         router.push({
-                        //             pathname: `/vaults/${vault?.vaultAddress}`,
-                        //             query: { user: currentAccount },
-                        //         })}>
-                        //         <VaultCard
-                        //             name={vault?.vaultName}
-                        //             address={vault?.vaultAddress}
-                        //             target={vault?.target}
-                        //             status={vault?.vaultStatus}
-                        //             amount={vault?.amountPledged}
-                        //             timestamp={vault?.timestamp}
-                        //             image="https://lh3.googleusercontent.com/b2fJSqKXfH9AJg63az3zmMUC6PMd_bmqnI5W-rtouKvZ03vBeiyayb3zqDq4t7PLt2HmNxcocUMjxb7V03Jy_mMZc_5wVDaxk_T5=w260"
-                        //         />
-
-                        //     </div>
-                        // </SwiperSlide>
-                        // <SwiperSlide>
-                            <div key={vault.vaultAddress} className='cursor-pointer rounded-xl' onClick={() =>
+                        <SwiperSlide>
+                            <div key={vault.vaultAddress} className='cursor-pointer' onClick={() =>
                                 router.push({
                                     pathname: `/vaults/${vault?.vaultAddress}`,
                                     query: { user: currentAccount },
                                 })}>
                                 <VaultCard
-                                    // name={vault?.vaultName}
+                                    name={vault?.vaultName}
                                     address={vault?.vaultAddress}
                                     target={vault?.target}
                                     status={vault?.vaultStatus}
-                                    amount={vault?.amount}
-                                    timestamp={vault?.fundraiseDuration}
-                                    creator={vault?.creator}
-                                    nfts={vault?.nfts}
-                                    tokenName={vault?.tokenName}
-                                    vaultName={vault?.vaultName}
-                                // image="https://lh3.googleusercontent.com/b2fJSqKXfH9AJg63az3zmMUC6PMd_bmqnI5W-rtouKvZ03vBeiyayb3zqDq4t7PLt2HmNxcocUMjxb7V03Jy_mMZc_5wVDaxk_T5=w260"
+                                    amount={vault?.amountPledged}
+                                    timestamp={vault?.timestamp}
+                                    image="https://lh3.googleusercontent.com/b2fJSqKXfH9AJg63az3zmMUC6PMd_bmqnI5W-rtouKvZ03vBeiyayb3zqDq4t7PLt2HmNxcocUMjxb7V03Jy_mMZc_5wVDaxk_T5=w260"
                                 />
 
                             </div>
+                        </SwiperSlide>
+                        // <SwiperSlide>
+                            // <div key={vault.vaultAddress} className='cursor-pointer rounded-xl' onClick={() =>
+                            //     router.push({
+                            //         pathname: `/vaults/${vault?.vaultAddress}`,
+                            //         query: { user: currentAccount },
+                            //     })}>
+                            //     <VaultCard
+                            //         // name={vault?.vaultName}
+                            //         address={vault?.vaultAddress}
+                            //         target={vault?.target}
+                            //         status={vault?.vaultStatus}
+                            //         amount={vault?.amount}
+                            //         timestamp={vault?.fundraiseDuration}
+                            //         creator={vault?.creator}
+                            //         nfts={vault?.nfts}
+                            //         tokenName={vault?.tokenName}
+                            //         vaultName={vault?.vaultName}
+                            //     // image="https://lh3.googleusercontent.com/b2fJSqKXfH9AJg63az3zmMUC6PMd_bmqnI5W-rtouKvZ03vBeiyayb3zqDq4t7PLt2HmNxcocUMjxb7V03Jy_mMZc_5wVDaxk_T5=w260"
+                            //     />
+
+                            // </div>
                         // </SwiperSlide>
                     ))}
 
-                {/* </Swiper> */}
+                </Swiper>
                 <div onClick={handleNext} className='cursor-pointer  bg-gray-300 rounded-full p-2 absolute -right-12  top-60 z-10'><ChevronRightIcon className='text-white h-7 w-7' /></div>
 
             </div >
