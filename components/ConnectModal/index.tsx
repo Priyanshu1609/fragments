f9a91dbff6a0affba6b8dc826df672ad2aab1c0a
xRAIDENx <priyanshupanda.ctp@gmail.com>
2022-10-11 11:30:10 +0530

    connect modal

--- components/ConnectModal/index.tsx
+++ components/ConnectModal/index.tsx
@@ -40,21 +40,21 @@ const ConnectModal: React.FC = () => {
             >
                 <p className='pr-24 mt-1 text-gra text-base font-extralight text-left font-montserrat'>Connect with one of the available wallet providers or create a new wallet.</p>
                 <div className='p-4 mt-6'>
-                    <div className='flex items-center justify-between  text-white hover:cursor-pointer'>
+                    <div className='flex items-center justify-evenly  text-white hover:cursor-pointer'>
                         <div
                             onClick={e => onConnect('metamask')}
-                            className='h-44 w-48 py-3 flex flex-col hover:bg-gray-700 space-x-2 justify-center items-center px-4 text-sm rounded-3xl border-[1px]  border-gray-600 text-left '
+                            className='h-44 w-48 py-3 flex flex-col  hover:bg-gray-700 space-x-2 justify-center items-center px-4 text-sm rounded-3xl !border-[1px]  !border-red-300 text-left '
                         >
                             <Image src={metamasklogo} height={80} width={80} />
                             <p className='font-black text-lg mt-1'>Metamask</p>
                         </div>
-                        <div
+                        {/* <div
                             onClick={() => onConnect('wallectconnect')}
                             className='h-44 w-48 py-3 flex flex-col hover:bg-gray-700 space-x-2 justify-center items-center px-4 text-sm rounded-3xl border-[1px]  border-gray-600 text-left '
                         >
                             <Image src={walletconnectlogo} height={70} width={120} />
                             <p className='font-black text-lg mt-3'>WalletConnect</p>
-                        </div>
+                        </div> */}
                     </div>
                     <p className='text-gray-400 text-sm text-left mt-4 font-montserrat'>Connecting your wallet does not give Fragments, access to your private keys or your funds.</p>
                 </div>
