2b1a2f28178c9719bf993bd755167c9fe491de88
xRAIDENx <priyanshupanda.ctp@gmail.com>
2022-10-15 10:54:25 +0530

    lifi

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from 'react'
 import type { NextPage } from 'next'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import Amplify, { Auth } from "aws-amplify";
 
 
 import { magic } from '../utils/magic';
