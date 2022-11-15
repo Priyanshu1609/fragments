20fec628913bbb21bc6f02107d6701fe94cb3cda
Ketan kudikyal <66976099+KetanKudikyal@users.noreply.github.com>
2022-11-15 20:10:10 +0530

    feat: added hotjar

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,14 @@
-
 import Layout from '../components/Layout'
 import '../styles/globals.css'
-import "@magiclabs/ui/dist/cjs/index.css";
-import { magic } from "../utils/magic";
+import '@magiclabs/ui/dist/cjs/index.css'
+import { magic } from '../utils/magic'
 
-import NProgress from "nprogress"
-import Router from "next/router"
+import NProgress from 'nprogress'
+import Router from 'next/router'
+import { hotjar } from 'react-hotjar'
 
-
-NProgress.configure({ showSpinner: false });
-Router.onRouteChangeStart = url => {
+NProgress.configure({ showSpinner: false })
+Router.onRouteChangeStart = (url) => {
   NProgress.start()
 }
 
@@ -18,33 +17,39 @@ Router.onRouteChangeComplete = () => NProgress.done()
 Router.onRouteChangeError = () => NProgress.done()
 
 async function refreshToken() {
-  const didToken = await magic.user.getIdToken();
-  const userMetadata = await magic.user.getMetadata();
-
+  const didToken = await magic.user.getIdToken()
+  const userMetadata = await magic.user.getMetadata()
 
   const body = JSON.stringify({
     didToken,
     issuer: userMetadata.issuer,
-  });
+  })
   const res = await fetch(
     `${awsconfig.aws_cloud_logic_custom[0].endpoint}/auth`,
     {
-      method: "POST",
+      method: 'POST',
       body,
     }
-  );
-  const json = await res.json();
+  )
+  const json = await res.json()
   return {
     identity_id: json.IdentityId,
     token: json.Token,
     expires_at: 2592000 * 1000 + new Date().getTime(),
-  };
+  }
 }
 
-
 export default function App({
-  Component, pageProps: { session, ...pageProps }
+  Component,
+  pageProps: { session, ...pageProps },
 }) {
+  React.useEffect(() => {
+    hotjar.initialize(3248630, 6)
+    hotjar.identify('USER_ID', { userProperty: 'value' })
+    hotjar.event('button-click')
+    hotjar.stateChange('/home')
+  }, [])
+
   return (
     <div>
       <Layout>
